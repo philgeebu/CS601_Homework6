1619a89cd2950727f48d22d704312c79f338b24f
Phil Gee <philgeejunk@gmail.com>
2021-12-13 21:04:57 -0500

    It works!

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <header>
+      <h1>Phil's Assignment 6</h1>
+    </header>
+    <section>
+      <p>Instructions: hover over the images to see a close-up.</p>
+      <HoverPicture v-for="picture in hoverList" :key="picture.id" :picture="picture" class="picture" />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import hoverJson from '@/assets/hover_pictures.json'
+  import HoverPicture from '@/components/HoverPicture.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+    name: 'App',
+    data() {
+      return {
+        hoverList: hoverJson
+      }
+    },
+    components: {
+      HoverPicture
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    font-family: sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+
+  h1,
+  p {
+    text-align: center;
+  }
+
+  .picture {
+    max-width: 500px;
+    border-radius: 7px;
+    cursor: zoom-in;
+  }
+
+  section {
+    border-radius: 7px;
+    gap: 1rem;
+    margin: 0 auto;
+    display: flex;
+    flex-flow: column wrap;
+    align-content: center;
+    box-shadow: 5px 5px 10px purple;
+    max-width: 960px;
+    padding: 2em;
+    margin-bottom: 30px;
+  }
 </style>
---
+++ src/components/HoverPicture.vue
@@ -0,0 +1,24 @@
+<template>
+  <img :src="pictureHover" @mouseover="hover = true" @mouseleave="hover = false">
+</template>
+
+<script>
+  export default {
+    name: 'HoverPicture',
+    data() {
+      return {
+        hover: false,
+      }
+    },
+    props: ['picture'],
+    computed: {
+      pictureHover() {
+        if (this.hover == true) {
+          return '/img/' + this.picture.picture_b_file
+        } else {
+          return '/img/' + this.picture.picture_a_file
+        }
+      }
+    },
+  }
+</script>
